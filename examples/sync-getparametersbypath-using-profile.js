c80125fb8c847b85101a1b7cb39f43d761023436
Richard Hyatt <richardhyatt@me.com>
2020-01-25 13:41:46 -0500

    allow region to get copied over when making sync calls

--- examples/sync-getparametersbypath-using-profile.js
+++ examples/sync-getparametersbypath-using-profile.js
@@ -2,12 +2,16 @@ const AWS = require( 'aws-sdk' );
 
 const { getParametersByPathSync } = require( '..'  /* 'aws-param-store' */ );
 
+// use a profile
 const credentials = new AWS.SharedIniFileCredentials( {profile: 'my-test-profile'} );
 AWS.config.credentials = credentials;
 
+// set the region
+AWS.config.update({region: 'us-east-1'});
+
 try {
 
-    let parameters = getParametersByPathSync( '/', { region: 'us-east-1' } );
+    let parameters = getParametersByPathSync( '/' );
 
     console.log( parameters );
 }
